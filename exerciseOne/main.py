f58e9b3168322ef042f5a8a5910aecc7bfdcd924
jhecking-10 <jhecking_10@protonmail.com>
2026-08-26 11:38:23 +0900

    added exerciseOne/

---
+++ exerciseOne/main.py
@@ -0,0 +1,14 @@
+def prompt():
+    name = input("What is your name?\n")
+    age = input("What is your age?\n")
+    current_year = input("What year is it now?\n")
+    return name, age, year
+
+# returns year the user will turn 100
+def calculate_year(name, age, year):
+    pass
+
+def main():
+    pass
+
+main()
